b47e4fbcce0a1842f69d88a19d0a12507bba9d23
uzairshafique <uzairshafique9@gmail.com>
2025-01-27 12:38:05 +0000

    Setting up the train/test split so it can be changed easily

--- lstm_files/lstm_yfinance.py
+++ lstm_files/lstm_yfinance.py
@@ -47,3 +47,7 @@ data.shape
 # MinMax scalar to normalise the values
 scaler = MinMaxScaler(feature_range= (0, 1))
 scaled_data = scaler.fit_transform(np.array(data).reshape(-1, 1))
+
+# 75% to Train , 25% to Test
+train_size = int(len(data)*.75)
+test_size = len(data) - train_size
