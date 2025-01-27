b1fee5a733574aadb6971046133bdc1288ebcd5e
uzairshafique <uzairshafique9@gmail.com>
2025-01-27 12:34:24 +0000

    create a ticker from yfinance

--- lstm_files/lstm_yfinance.py
+++ lstm_files/lstm_yfinance.py
@@ -16,3 +16,11 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 
+ticker = 'AAPL'
+
+df = yf.download(ticker, 
+                 start = '2000-01-01',
+                 end = '2024-12-31',
+                 multi_level_index = False)
+
+info = yf.Ticker(ticker)
