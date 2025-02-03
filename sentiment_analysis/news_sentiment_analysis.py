a68abb57c3907e605b919fd21cc90c5a099ebbf0
uzairshafique <uzairshafique9@gmail.com>
2025-02-03 20:11:28 +0000

    Setting up the newsdata api response and yfinance response

--- sentiment_analysis/news_sentiment_analysis.py
+++ sentiment_analysis/news_sentiment_analysis.py
@@ -7,3 +7,10 @@ MODEL = "deepseek-r1:7b"
 api = NewsDataApiClient(apikey='PUT API KEY HERE')
 STOCK = "Starbucks"
 ticker = yf.Ticker('SBUX')
+
+news_data = ""
+
+response = api.latest_api(q='{stock} stock'.format(stock = STOCK))
+
+# get list of news
+news = yf.Search("{stock}".format(stock = STOCK), news_count=20).news
