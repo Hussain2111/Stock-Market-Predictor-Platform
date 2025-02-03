361618c9a46cd01105585ce9bbc3a5d55b5e65e1
uzairshafique <uzairshafique9@gmail.com>
2025-02-03 20:12:42 +0000

    Extracting the news descriptions from newsdata

--- sentiment_analysis/news_sentiment_analysis.py
+++ sentiment_analysis/news_sentiment_analysis.py
@@ -10,7 +10,15 @@ ticker = yf.Ticker('SBUX')
 
 news_data = ""
 
+# Get the news response from newsdata api
 response = api.latest_api(q='{stock} stock'.format(stock = STOCK))
 
 # get list of news
 news = yf.Search("{stock}".format(stock = STOCK), news_count=20).news
+
+# Extract individual descriptions
+for article in response["results"]:
+        article_description = article.get('description')
+        if isinstance(article_description, str):
+            news_data += article.get('description')
+
