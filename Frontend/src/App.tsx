01e034788b2fb26db9f7707535836b211044bb1f
Shayaan9559 <ss2697@kent.ac.uk>
2025-03-14 20:13:59 +0000

    adding functionality for predictionprovider

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,17 +3,20 @@ import HomePage from "./Home";
 import AnalysisPage from "./analysis";
 import Trading from "./Trading";
 import Account from "./setting";
+import { PredictionProvider } from "./context/PredictionContext";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/analysis" element={<AnalysisPage />} />
-        <Route path="/trade" element={<Trading />} />
-        <Route path="/settings" element={<Account />} />
-      </Routes>
-    </Router>
+    <PredictionProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/analysis" element={<AnalysisPage />} />
+          <Route path="/trade" element={<Trading />} />
+          <Route path="/settings" element={<Account />} />
+        </Routes>
+      </Router>
+    </PredictionProvider>
   );
 }
 
