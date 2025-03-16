595969fb8b8e6dc71beeeea6be6a352744281f80
Uzair Shafique <uas4@kent.ac.uk>
2025-03-16 23:37:34 +0000

    Added the total value of stocks

--- Frontend/src/Portfolio.tsx
+++ Frontend/src/Portfolio.tsx
@@ -6,6 +6,7 @@ import { DollarSign, TrendingUp } from "lucide-react";
 interface Stock {
   ticker: string;
   quantity: number;
+  total_value: number;
 }
 
 const Portfolio = () => {
@@ -94,7 +95,9 @@ const Portfolio = () => {
                 </p>
                 <div className="flex items-center mt-4">
                   <DollarSign className="text-yellow-400 w-5 h-5 mr-2" />
-                  <p className="text-lg font-semibold"></p>
+                  <p className="text-lg font-semibold">
+                    ${stock.total_value?.toFixed(2) || "N/A"}
+                  </p>
                 </div>
               </div>
             ))}
