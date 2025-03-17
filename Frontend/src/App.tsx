d1536d75ee8a885c1c9aaffba549da639397bd11
sa2091 <sa2091@kent.ac.uk>
2025-03-17 22:52:26 +0000

    Fixing error with "no routes found with home"

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -13,6 +13,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/home" element={<HomePage />} />
           <Route path="/analysis" element={<AnalysisPage />} />
           <Route path="/trade" element={<Trading />} />
           <Route path="/settings" element={<Account />} />
